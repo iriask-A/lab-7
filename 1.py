db83a638e2a4fabd38827f620674b82cd432416e
Ingkar <i_askarbekova@kbtu.kz>
2025-03-17 20:56:00 +0500

    clock

--- 1.py
+++ 1.py
@@ -1,28 +1,36 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
-clock_img = pygame.image.load("clock.jpg.webp").convert() 
-clock_arrow1 = pygame.image.load("arrow1.jpg").convert() 
-clock_arrow2 = pygame.image.load("arrow2.jpg").convert() 
-clock_arrow1.set_colorkey((255, 255, 255))
-clock_arrow2.set_colorkey((255, 255, 255))
-a=pygame.transform.scale(clock_arrow1,(100,150))
-b=pygame.transform.scale(clock_arrow2,(100,120))
-
-
-running = True 
-
-while running: 
+screen = pygame.display.set_mode((800, 600))
+clock_img = pygame.image.load("clock.png").convert()
+min_hand = pygame.image.load("min_hand.png").convert()
+sec_hand = pygame.image.load("sec_hand.png").convert()
+min_hand.set_colorkey((0, 0, 0))
+sec_hand.set_colorkey((0, 0, 0))
+min_hand = pygame.transform.scale(min_hand, (600, 600))  
+sec_hand = pygame.transform.scale(sec_hand, (600, 600))  
+min_angle = 0  
+sec_angle = 0  
+clock = pygame.time.Clock()
+running = True
+while running:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             running = False
-
+    rotated_min_hand = pygame.transform.rotate(min_hand, min_angle)
+    rotated_sec_hand = pygame.transform.rotate(sec_hand, sec_angle)
+    min_rect = rotated_min_hand.get_rect(center=(400, 300))
+    sec_rect = rotated_sec_hand.get_rect(center=(400, 300))
     screen.fill((255, 255, 255))
     screen.blit(clock_img, (0, 0))
-    screen.blit(a, (275, 130))
-    screen.blit(b, (255, 180))
+    screen.blit(rotated_min_hand, min_rect.topleft)
+    screen.blit(rotated_sec_hand, sec_rect.topleft)
+
     pygame.display.flip()
+
+    min_angle -= 1 
+    sec_angle -= 6 
+
+    clock.tick(60)
 
 pygame.quit()
